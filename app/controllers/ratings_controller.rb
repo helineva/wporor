e3ddcec6c1acb771f54359f9819c0eda3c54c186
Teemu Helineva <teemu.helineva@gmail.com>
2018-09-11 17:56:46 +0300

    Tehty tehtävä 2.11.

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -9,8 +9,13 @@ class RatingsController < ApplicationController
   end
 
   def create
-    raise
     Rating.create params.require(:rating).permit(:score, :beer_id)
+    redirect_to ratings_path
+  end
+
+  def destroy
+    rating = Rating.find(params[:id])
+    rating.delete
     redirect_to ratings_path
   end
 end
