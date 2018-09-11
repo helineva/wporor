a647818b4975e5c994132e1bb9f8d342c8a40f03
Teemu Helineva <teemu.helineva@gmail.com>
2018-09-11 18:43:15 +0300

    Tehty tehtävä 2.14.

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,6 @@
 class Brewery < ActiveRecord::Base
   has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
 
   def print_report
     puts name
@@ -10,5 +11,12 @@ class Brewery < ActiveRecord::Base
   def restart
     self.year = 2018
     puts "changed year to #{year}"
+  end
+
+  def average_rating
+    count = self.ratings.count
+    return nil if count == 0
+    total = self.ratings.reduce(0) {|sum,r| sum + r.score}
+    return total.fdiv(count).round(2)
   end
 end
