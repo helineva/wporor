efd0c08b12f675f5c78ab37b5972d9a1d2ba072f
Teemu Helineva <teemu.helineva@gmail.com>
2018-09-11 16:09:09 +0300

    Tehty tehtävä 2.4.

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,12 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    count = self.ratings.count
+    return nil if count == 0
+    total = self.ratings.reduce(0) {|sum,r| sum + r.score}
+    return total.fdiv(count).round(2)
+  end
+
 end
