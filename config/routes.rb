8266e5e1eb5874e6893ad26b439f9e394e389cfb
Teemu Helineva <teemu.helineva@gmail.com>
2018-09-11 15:13:40 +0300

    Lisätty navibar. Tehty reititystä. Luotu Rating.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :beers
   resources :breweries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'breweries#index'
+  get 'ratings', to: 'ratings#index'
 end
