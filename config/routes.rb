737922960a32227cef2b098ca9efce94df2d7c1a
Teemu Helineva <teemu.helineva@gmail.com>
2018-09-17 01:45:00 +0300

    Tehty tehtävä 3.4.

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   root 'breweries#index'
   resources :ratings, only: [:index, :new, :create, :destroy]
   get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
   resource :session, only: [:new, :create, :destroy]
 end
