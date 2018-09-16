5d27262df772d5c9f58520664f651908de8dfb5b
Teemu Helineva <teemu.helineva@gmail.com>
2018-09-17 01:24:41 +0300

    Tehty tehtävä 3.3.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   resources :beers
   resources :breweries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'breweries#index'
   resources :ratings, only: [:index, :new, :create, :destroy]
+  get 'signup', to: 'users#new'
+  resource :session, only: [:new, :create, :destroy]
 end
