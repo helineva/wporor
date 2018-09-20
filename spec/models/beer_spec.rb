811a2af87cbbe2867bcb1269a61d81b6b39164a0
Teemu Helineva <teemu.helineva@gmail.com>
2018-09-20 20:47:23 +0300

    Tehty tehtävä 4.2.

---
+++ spec/models/beer_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Beer, type: :model do
+  it "is saved with a name, style and brewery" do
+    test_brewery = Brewery.create name:"testbrewery", year:1900
+    test_beer = Beer.create name:"testbeer", style:"teststyle", brewery:test_brewery
+
+    expect(test_beer.valid?).to be(true)
+    expect(Beer.count).to eq(1)
+  end
+
+  it "is not saved if name is not given" do
+    test_brewery = Brewery.create name:"testbrewery", year:1900
+    test_beer = Beer.create style:"teststyle", brewery:test_brewery
+
+    expect(test_beer.valid?).to be(false)
+    expect(Beer.count).to eq(0)
+  end
+
+  it "is not saved if style is not given" do
+    test_brewery = Brewery.create name:"testbrewery", year:1900
+    test_beer = Beer.create name:"testbeer", brewery:test_brewery
+
+    expect(test_beer.valid?).to be(false)
+    expect(Beer.count).to eq(0)
+  end
+end
