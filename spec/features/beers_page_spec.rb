839249be89c19176a9daf853099275dcebefc4f4
Teemu Helineva <teemu.helineva@gmail.com>
2018-09-24 21:41:50 +0300

    Tehty tehtävä 4.15.

--- spec/features/beers_page_spec.rb
+++ spec/features/beers_page_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
+include Helpers
+
 describe "Beers page" do
   let!(:brewery) { FactoryBot.create :brewery, name:"Koff" }
+  let!(:user) { FactoryBot.create :user }
+
+  before :each do
+    sign_in(username:"Pekka", password:"Foobar1")
+  end
 
   it "is saved if name is valid" do
     visit new_beer_path
